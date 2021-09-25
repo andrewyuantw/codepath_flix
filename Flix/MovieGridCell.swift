4df1320374f921fc969e480690f263da1ab0008d
Andrew Yuan <ayuan124@umd.edu>
2021-09-24 21:58:45 -0400

    Week 2 Required

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Andrew Yuan on 9/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
